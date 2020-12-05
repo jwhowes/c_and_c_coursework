9a80b2d5c8d8b351bc56d7aad00f2e9ea5171e82
jwhowes <jacob.w.howes@durham.ac.uk>
2020-12-05 17:59:04 +0000

    Implemented lz77 (it's kind of rubbish)

---
+++ compression/decoder.py
@@ -0,0 +1,35 @@
+import numpy as np
+import math
+import sys
+import json
+from bitstring import *
+import binascii
+
+W = 2**8 - 1
+L = 2**8 - 1
+W_bits = math.ceil(math.log(W + 1, 2))
+L_bits = math.ceil(math.log(L + 1, 2))
+character_bits = 16
+max_character_bits = 8
+
+ifile = open("file.bin", "rb")
+b = ifile.read()
+ifile.close()
+
+enc = ""
+for i in b:
+	enc += np.binary_repr(i).zfill(max_character_bits)
+
+def lz77_decode(enc):
+	m = ""
+	for j in range(0, len(enc), W_bits + L_bits + character_bits):
+		if enc[j + W_bits + L_bits : j + W_bits + L_bits + character_bits] != "":
+			i = len(m)
+			d = int(enc[j : j + W_bits], 2)
+			l = int(enc[j + W_bits : j + W_bits + L_bits], 2)
+			c = chr(int(enc[j + W_bits + L_bits : j + W_bits + L_bits + character_bits], 2))
+			m += m[i - d : i - d + l] + c
+	return m
+ofile = open("out.tex", "w")
+ofile.write(lz77_decode(enc))
+ofile.close()
