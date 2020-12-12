00e1b8bdecf05e57d232ad091da52e967d3478b3
jwhowes <jacob.w.howes@durham.ac.uk>
2020-12-12 11:01:46 +0000

    Now prints characters and probabilities, just need to implement arithmetic encoder

--- compression/ppmc_encoder.py
+++ compression/ppmc_encoder.py
@@ -31,12 +31,38 @@ class Trie:
 			for c in self.children:
 				if C[depth] == c.character:
 					c.add_character(C, S, depth + 1, con_string + self.character)
-# IT WORKS!!!!!
+	def get_code(self, C, S, c_length, c_pos):
+		denominator = 0
+		node = None
+		low = 0
+		if c_length == -1:
+			print(S, "order -1")
+			return
+		if c_pos == 0:
+			for c in self.children:
+				if c.character == S:
+					node = c
+				if node is None:
+					low += c.frequency
+				denominator += c.frequency
+			denominator += len(self.children)
+			if node is not None:
+				print(node.character, "low:", str(low) + "/" + str(denominator), "high:", str(low + node.frequency) + "/" + str(denominator))
+				return
+		else:
+			for c in self.children:
+				if c.character == C[-c_pos]:
+					c.get_code(C, S, c_length, c_pos - 1)
+					return
+				denominator += c.frequency
+			denominator += len(self.children)
+		print("escape", "low:", str(denominator - len(self.children)) + "/" + str(denominator), "high:", 1)
+		root.get_code(C, S, c_length - 1, c_length - 1)
+# Currently it just prints what's encoded and with what probability but it is correct
 root = Trie("")
 
 for i in range(len(m)):
 	C = m[max(0, i - N) : i]
+	root.get_code(C, m[i], len(C), len(C))
 	root.add_character(C, m[i])
-
-for c in n.children:
-	print(c.character, c.frequency)
+	print("!")
