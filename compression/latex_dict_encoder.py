988ee80fd74e1c2e4bf9adf7d79846285844a329
jwhowes <jacob.w.howes@durham.ac.uk>
2020-12-19 17:19:08 +0000

    Reworked latex dictionary compression to be more flexible (dictionary can no just be a list of arbitrary strings)

--- compression/latex_dict_encoder.py
+++ compression/latex_dict_encoder.py
@@ -3,36 +3,45 @@ import math
 import sys
 import csv
 
-ifile = open("in.tex", "rb")
-m = ifile.read()
-ifile.close()
+dict_file = open("latex_dict.txt", "r")
+latex_commands = dict_file.readlines()
+dict_file.close()
+
+for i in range(len(latex_commands)):
+	latex_commands[i] = latex_commands[i].replace("\n", "")
+	latex_commands[i] = latex_commands[i].replace("\r", "")
+	latex_commands[i] = latex_commands[i].encode()
 
 latex_dict = {}
 
-with open("latex_dict.csv", newline='\n') as dict_file:
-	reader = csv.reader(dict_file, delimiter=',')
-	for row in reader:
-		if len(row[1]) > 0:
-			latex_dict[row[0].encode()] = int(row[1]).to_bytes(2, 'big')
+for i in range(len(latex_commands)):
+	latex_dict[latex_commands[i]] = (i + 33024).to_bytes(2, 'big')
+
+ifile = open("in.tex", "rb")
+m = ifile.read()
+ifile.close()
 
 enc = bytearray()
 
 i = 0
 while i < len(m):
 	if m[i] == 92:
-		found = False
+		longest_match = None
 		for k in latex_dict:
-			if i + len(k) <= len(m) and m[i : i + len(k)] == k:
-				found = True
-				enc += latex_dict[k]
-				i += len(k)
-				break
-		if not found:
+			if i + len(k) <= len(m) and m[i : i + len(k)] == k and (longest_match is None or len(k) > len(longest_match)):
+				longest_match = k
+		if longest_match is None:
 			enc.append(m[i])
 			i += 1
+		else:
+			enc += latex_dict[longest_match]
+			i += len(longest_match)
 	else:
 		enc.append(m[i])
 		i += 1
+
+print(len(m), len(enc))
+print(m, enc)
 
 ofile = open("dict_encoder_out.lz", "wb")
 ofile.write(enc)
