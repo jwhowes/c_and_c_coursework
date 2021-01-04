50c0bcfdefef13db2fc84984617da7c33ce25e28
jwhowes <jacob.w.howes@durham.ac.uk>
2021-01-04 19:28:46 +0000

    Made key 56 bits

--- cryptography/des_brute_force/des_brute_force/kernel.cu
+++ cryptography/des_brute_force/des_brute_force/kernel.cu
@@ -14,35 +14,23 @@ using namespace std;
 
 __global__ void brute_force_kernel(uint64_t plaintext, uint64_t ciphertext, uint64_t * res_key, bool * done) {
 	// Generate first 3 bytes of the thread's key
-	uint64_t thread_id = (uint64_t)(blockIdx.x * blockDim.x + threadIdx.x);
-	uint64_t thread_key = 0;
-	uint64_t bit_mask = 0x7f;
+	uint64_t thread_key = (uint64_t)(blockIdx.x * blockDim.x + threadIdx.x);
 	uint64_t keys[16];
 	uint64_t PC1_permuted;
 	uint64_t C;
 	uint64_t D;
 	int v;
-	for (int i = 0; i < 3; i++) {
-		uint64_t masked = thread_id & bit_mask;
-		thread_key |= masked | (parities[masked] << 7);
-		thread_key <<= 8;
-		thread_id >>= 7;
-	}
-	thread_key <<= 32;
+	thread_key <<= 35;
+	//thread_key <<= 32;
 	// First three bytes of the thread key are now fixed
 	for (uint64_t i = 0; i < thread_encryptions; i++) {
-		thread_key >>= 40;
-		uint64_t k = i;
-		for (int j = 0; j < 5; j++) {
-			uint64_t masked = k & bit_mask;
-			thread_key <<= 8;
-			thread_key |= masked | (parities[masked] << 7);
-			k >>= 7;
-		}
+		thread_key >>= 35;
+		thread_key <<= 35;
+		thread_key |= i;
 		// Encrypt plaintext with thread_key
 		// First, obtain key schedule
-		permute(thread_key, PC_1, &PC1_permuted, 64);
-		split_64(PC1_permuted, &C, &D);
+		permute(thread_key, PC_1, &PC1_permuted, 56);
+		/*split_64(PC1_permuted, &C, &D);  Need to rewrite now that thread_key is 56 bits
 		for (int j = 1; j <= 16; j++) {
 			if (j == 1 || j == 2 || j == 9 || j == 16) {
 				v = 1;
@@ -52,7 +40,7 @@ __global__ void brute_force_kernel(uint64_t plaintext, uint64_t ciphertext, uint
 			permute((C << 32) | D, PC_2, &keys[j - 1], 64);
 			cycle_left(&C, v, 32);
 			cycle_left(&D, v, 32);
-		}
+		}*/
 	}
 }
 
