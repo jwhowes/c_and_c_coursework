36b8009d6000401fc7de5891783cbedf287d711e
jwhowes <jacob.w.howes@durham.ac.uk>
2021-01-06 16:45:10 +0000

    Done something that I think may improve the keys per sec

--- cryptography/des_brute_force/des_brute_force/kernel.cu
+++ cryptography/des_brute_force/des_brute_force/kernel.cu
@@ -1,7 +1,4 @@
-﻿
-#include <ctime>
-
-#include "cuda_runtime.h"
+﻿#include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
 #include <iostream>
@@ -77,9 +74,9 @@ __device__ void des_encrypt(uint64_t block, uint64_t * keys, uint64_t * ret) {
 	*ret = (right << 32) | left;
 }
 
-__global__ void brute_force_kernel(uint64_t plaintext, uint64_t ciphertext, uint64_t * res_key, bool * done) {
+__global__ void brute_force_kernel(uint64_t plaintext, uint64_t ciphertext, uint64_t block_key, uint64_t * res_key, bool * done) {
 	// Generate first 3 bytes of the thread's key
-	uint64_t thread_key = (uint64_t)(blockIdx.x * blockDim.x + threadIdx.x) << 35;
+	uint64_t thread_key = block_key + ((uint64_t)(blockIdx.x * blockDim.x + threadIdx.x) << 35);
 	//const uint64_t bit_mask = 0x00FFFFF800000000;
 	uint64_t keys[16];
 	uint64_t PC1_permuted;
@@ -88,31 +85,29 @@ __global__ void brute_force_kernel(uint64_t plaintext, uint64_t ciphertext, uint
 	uint64_t temp;
 	int v;
 	// First three bytes of the thread key are now fixed
-	for (uint64_t i = 0; i < thread_encryptions; i++) {
-		if (*done) {
-			return;
+	if (*done) {
+		return;
+	}
+	// Encrypt plaintext with thread_key
+	// First, obtain key schedule
+	permute(thread_key, PC_1, &PC1_permuted, 56, 56);
+	split_56(PC1_permuted, &C, &D);
+	for (int j = 1; j <= 16; j++) {
+		if (j == 1 || j == 2 || j == 9 || j == 16) {
+			v = 1;
+		}else {
+			v = 2;
 		}
-		// Encrypt plaintext with thread_key
-		// First, obtain key schedule
-		permute(thread_key, PC_1, &PC1_permuted, 56, 56);
-		split_56(PC1_permuted, &C, &D);
-		for (int j = 1; j <= 16; j++) {
-			if (j == 1 || j == 2 || j == 9 || j == 16) {
-				v = 1;
-			}else {
-				v = 2;
-			}
-			permute((C << 28) | D, PC_2, &keys[j - 1], 48, 56);
-			cycle_left(&C, v, 28);
-			cycle_left(&D, v, 28);
-		}
-		des_encrypt(plaintext, keys, &temp);
-		if (temp == ciphertext) {
-			*done = true;
-			*res_key = ciphertext;
-			return;
-		}
-		thread_key++;
+		permute((C << 28) | D, PC_2, &keys[j - 1], 48, 56);
+		cycle_left(&C, v, 28);
+		cycle_left(&D, v, 28);
+	}
+	des_encrypt(plaintext, keys, &temp);
+
+	if (temp == ciphertext) {
+		*done = true;
+		*res_key = ciphertext;
+		return;
 	}
 }
 
@@ -132,7 +127,10 @@ int main() {
 	cudaMalloc(&res_key, sizeof(uint64_t));
 	cudaMalloc(&done, sizeof(bool));
 	cudaMemcpy(done, false, sizeof(bool), cudaMemcpyHostToDevice);
-	brute_force_kernel <<<2048, 1024>>> (plaintext, ciphertext, res_key, done);
+	for (uint64_t i = 0; i < (uint64_t)1 << 46; i++) {
+		brute_force_kernel <<<1, 1024>>> (plaintext, ciphertext, i << 46, res_key, done);
+	}
+	printf("All blocks started\n");
 	cudaDeviceSynchronize();
 	return 0;
 }
